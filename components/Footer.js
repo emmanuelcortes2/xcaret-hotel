c769c41f39693976b43f18f7edabf6075bcd263b
emmanuelcortes2 <emmanuelcortes2@hotmail.com>
2023-09-17 15:49:12 -0600

    footer main layout & styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,19 @@
 'use client'
 import { useEffect, useState } from 'react'
-import * as data from '../data/data.json'
+import data from '../data/data.json' assert { type: 'json' }
 
 export default function Footer() {
-  // const [data, setData] = useState(null)
-  // useEffect(() => {
-  //   const fetchedData = async () => {
-  //     const res = await fetch(
-  //       'https://raw.githubusercontent.com/javialcocer/test-json/main/data.json'
-  //     )
-  //     const data = await res.json()
-  //     console.log('data', data)
-  //     setData(data)
-  //   }
-
-  //   fetchedData()
-  // }, [])
+  const [datos, setDatos] = useState(null)
+  useEffect(() => {
+    setDatos(data)
+  }, [])
   return (
-    <footer className="flex flex-col text-white bg-gray-400">
-      {data && (
+    <footer className="flex flex-col text-white bg-black p-4">
+      {datos && (
         <>
-          <p className="text-center">{data.es.footer.copy}</p>
+          <p className="text-center">{datos.es.footer.copy}</p>
           <ul className="flex flex-row justify-between p-4">
-            {data.es.footer.links.map((item, index) => (
+            {datos.es.footer.links.map((item, index) => (
               <li key={index}>
                 <a href={item.href}>{item.tittle}</a>
               </li>
